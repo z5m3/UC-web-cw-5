54240abb726e8aa25f5033ec94f8f075ae08adf7
z5m3 <142611522+z5m3@users.noreply.github.com>
2023-09-04 19:50:50 +0300

    DONE

---
+++ java.js
@@ -0,0 +1,17 @@
+alert("مرحبا بك في موقعي! انا سلطان")
+let grade = prompt ("ادخل درجتك")
+console.log("%c GRADE",
+"color: blue")
+if(grade>=90&& grade<= 100){
+alert('لقد حصلت على امتياز')} 
+else if(grade>=50&& grade<=50){
+alert('راسب')}
+if(grade>=80&& grade<= 89){ 
+    alert('لقد حصلت على جيد جدا')}
+    if( grade>=70&& grade<=79){
+        alert('لقد حصلت على جيد') }
+        if( grade>=60&& grade<= 69){
+            alert('لقد حصلت على مقبول')  }
+            if( grade>=50&& grade<= 59){
+                alert(' لقد حصلت على ضعيف') }
+    
